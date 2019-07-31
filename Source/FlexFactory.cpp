15013bdf82b38fb027976fe7d5c37e5bb71f19a1
ChiMon <Chi-Mon.NoelLam@komplett.com>
2019-08-01 01:47:20 +0200

    [patch] cosmetic

--- Source/FlexFactory.cpp
+++ Source/FlexFactory.cpp
@@ -7,27 +7,27 @@ namespace FlexItemFactory
 	FlexItem makeButtonBoxItem(Component& component)
 	{
 		return FlexItem(component)
-			.withMinHeight(ComponentSizes::StepButtonHeight)
-			.withMinWidth(ComponentSizes::StepButtonWidth)
-			.withMaxHeight(ComponentSizes::StepButtonHeight)
-			.withMaxWidth(ComponentSizes::StepButtonWidth);
+			.withMinHeight	(ComponentSizes::StepButtonHeight)
+			.withMinWidth	(ComponentSizes::StepButtonWidth)
+			.withMaxHeight	(ComponentSizes::StepButtonHeight)
+			.withMaxWidth	(ComponentSizes::StepButtonWidth);
 	}
 
 	FlexItem makeEncoderBoxItem(Component& component)
 	{
 		return FlexItem(component)
-			.withMinHeight(ComponentSizes::StepEncoderHeight)
-			.withMinWidth(ComponentSizes::StepEncoderWidth)
-			.withMaxHeight(ComponentSizes::StepEncoderHeight)
-			.withMaxWidth(ComponentSizes::StepEncoderWidth);
+			.withMinHeight	(ComponentSizes::StepEncoderHeight)
+			.withMinWidth	(ComponentSizes::StepEncoderWidth)
+			.withMaxHeight	(ComponentSizes::StepEncoderHeight)
+			.withMaxWidth	(ComponentSizes::StepEncoderWidth);
 	}
 
 	FlexItem makeUnderStepButtonsPanelItem(Component& component)
 	{
 		return FlexItem(component)
-			.withMinHeight(ComponentSizes::UnderStepButtonsPanelHeight)
-			.withMaxHeight(ComponentSizes::UnderStepButtonsPanelHeight)
-			.withAlignSelf(FlexItem::AlignSelf::center);
+			.withMinHeight	(ComponentSizes::UnderStepButtonsPanelHeight)
+			.withMaxHeight	(ComponentSizes::UnderStepButtonsPanelHeight)
+			.withAlignSelf	(FlexItem::AlignSelf::center);
 	}
 }
 
@@ -44,8 +44,8 @@ namespace  FlexBoxFactory
 	FlexBox maketEncodersBox()
 	{
 		FlexBox box;
-		box.justifyContent = FlexBox::JustifyContent::center;
-		box.alignContent = FlexBox::AlignContent::flexEnd;
+		box.justifyContent	= FlexBox::JustifyContent::center;
+		box.alignContent	= FlexBox::AlignContent::flexEnd;
 		return box;
 	}
 
